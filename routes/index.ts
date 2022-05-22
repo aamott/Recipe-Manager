497642a2f87769a716066cbd6338c99650ee9a08
aamott <adam.amott@gmail.com>
2022-05-21 22:28:15 -0600

    added proper import for swagger json

--- routes/index.ts
+++ routes/index.ts
@@ -12,7 +12,9 @@ router.use('/recipes', require('./recipes'));
  * Swagger
  */
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
+// const swaggerDocument = require('../swagger.json');
+import * as swaggerDocument from "../swagger.json";
+
 
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
