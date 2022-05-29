9d9465012359004ea01c18df1f4c4cb31a6cdfc0
aamott <adam.amott@gmail.com>
2022-05-28 22:15:07 -0600

    Added try catch

--- controllers/recipes.ts
+++ controllers/recipes.ts
@@ -7,11 +7,15 @@ const mongodb = require('../db/connect');
 
 // Get all recipes
 const getAllRecipes = async (req: any, res: any) => {
-  const result = await mongodb.getDb().db().collection('recipes').find();
-  result.toArray().then((lists: any) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send(JSON.stringify(lists));
-  });
+  try {
+    const result = await mongodb.getDb().db().collection('recipes').find();
+    result.toArray().then((lists: any) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).send(JSON.stringify(lists));
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 // Get a specific recipe
@@ -19,14 +23,18 @@ const getRecipe = async (req: any, res: any) => {
   /*
     #swagger.description = "Get a single recipe by id"
   */
-  const recipeId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('recipes').find({
-    _id: recipeId
-  });
-  result.toArray().then((lists: any[]) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const recipeId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('recipes').find({
+      _id: recipeId
+    });
+    result.toArray().then((lists: any[]) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 // Add a new recipe
@@ -35,38 +43,42 @@ const addRecipe = async (req: any, res: any, _next: any) => {
     #swagger.description = "Create a new recipe. Returns the new recipe id."
   */
   //  Check required fields
-  if (
-    !req.body.category ||
-    !req.body.tags ||
-    !req.body.description ||
-    !req.body.title ||
-    !req.body.ingredients ||
-    !req.body.instructions
-  ) {
+  try {
+    if (
+      !req.body.category ||
+      !req.body.tags ||
+      !req.body.description ||
+      !req.body.title ||
+      !req.body.ingredients ||
+      !req.body.instructions
+    ) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(500).json({ error: 'Missing required fields' });
+      return;
+    }
+
+    //  Add recipe to database
+    const newRecipe = {
+      title: req.body.title,
+      category: req.body.category,
+      description: req.body.description,
+      servings: req.body.servings || null,
+      prepTime: req.body.prepTime || null,
+      cookTime: req.body.cookTime || null,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+      tags: req.body.tags,
+      createdDate: new Date(),
+      updatedDate: new Date()
+    };
+    const result = await mongodb.getDb().db().collection('recipes').insertOne(newRecipe);
+
+    //  Return new recipe id
     res.setHeader('Content-Type', 'application/json');
-    res.status(500).json({ error: 'Missing required fields' });
-    return;
+    res.status(201).json(result.insertedId);
+  } catch (error) {
+    res.status(500).send(error);
   }
-
-  //  Add recipe to database
-  const newRecipe = {
-    title: req.body.title,
-    category: req.body.category,
-    description: req.body.description,
-    servings: req.body.servings || null,
-    prepTime: req.body.prepTime || null,
-    cookTime: req.body.cookTime || null,
-    ingredients: req.body.ingredients,
-    instructions: req.body.instructions,
-    tags: req.body.tags,
-    createdDate: new Date(),
-    updatedDate: new Date()
-  };
-  const result = await mongodb.getDb().db().collection('recipes').insertOne(newRecipe);
-
-  //  Return new recipe id
-  res.setHeader('Content-Type', 'application/json');
-  res.status(201).json(result.insertedId);
 };
 
 // Update a recipe
@@ -74,64 +86,62 @@ const updateRecipe = async (req: any, res: any) => {
   /*
     #swagger.description = "Update a contact"
   */
-  const recipeId = new ObjectId(req.params.id);
-  //  Add recipe to database
-  const update = {
-    title: req.body.title,
-    category: req.body.category,
-    description: req.body.description,
-    servings: req.body.servings || null,
-    prepTime: req.body.prepTime || null,
-    cookTime: req.body.cookTime || null,
-    ingredients: req.body.ingredients,
-    instructions: req.body.instructions,
-    tags: req.body.tags,
-    createdDate: new Date(),
-    updatedDate: new Date()
-  };
-  const result = await mongodb.getDb().db().collection('contacts').updateOne(
-    {
-      _id: recipeId
-    },
-    {
-      $set: update
+  try {
+    const recipeId = new ObjectId(req.params.id);
+    //  Add recipe to database
+    const update = {
+      title: req.body.title,
+      category: req.body.category,
+      description: req.body.description,
+      servings: req.body.servings || null,
+      prepTime: req.body.prepTime || null,
+      cookTime: req.body.cookTime || null,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+      tags: req.body.tags,
+      createdDate: new Date(),
+      updatedDate: new Date()
+    };
+    const result = await mongodb.getDb().db().collection('contacts').updateOne(
+      {
+        _id: recipeId
+      },
+      {
+        $set: update
+      }
+    );
+    res.setHeader('Content-Type', 'application/json');
+    if (result.modifiedCount === 1) {
+      res.status(200).json(result.modifiedCount);
+    } else {
+      res.status(500).json(result.error || 'An error occured while updating contact');
     }
-  );
-  res.setHeader('Content-Type', 'application/json');
-  if (result.modifiedCount === 1) {
-    res.status(200).json(result.modifiedCount);
-  } else {
-    res.status(500).json(result.error || 'An error occured while updating contact');
+  } catch (error) {
+    res.status(500).send(error);
   }
 };
 
 // Delete a recipe
-const deleteRecipe = async (
-  req: any,
-  res: {
-    setHeader: (arg0: string, arg1: string) => void;
-    status: (arg0: number) => {
-      (): any;
-      new (): any;
-      json: { (arg0: { error: string }): void; new (): any };
-    };
-  }
-) => {
+const deleteRecipe = async ( req: any, res: any ) => {
   /*
     #swagger.description = "Delete a recipe"
   */
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('recipes').deleteOne({
-    _id: userId
-  });
-  res.setHeader('Content-Type', 'application/json');
-
-  if (result.deletedCount === 1) {
-    res.status(200).json(result.deletedCount);
-  } else {
-    res.status(404).json({
-      error: 'No recipe found'
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('recipes').deleteOne({
+      _id: userId
     });
+    res.setHeader('Content-Type', 'application/json');
+
+    if (result.deletedCount === 1) {
+      res.status(200).json(result.deletedCount);
+    } else {
+      res.status(404).json({
+        error: 'No recipe found'
+      });
+    }
+  } catch (error) {
+    res.status(500).send(error);
   }
 };
 
