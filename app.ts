4bcf21ee2e8f1f96b25241c074db5d239d386327
aamott <adam.amott@gmail.com>
2022-06-13 17:40:41 -0600

    added authentication

--- app.ts
+++ app.ts
@@ -2,15 +2,60 @@
  * Entry point for the app
  */
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongodb = require('./db/connect');
+const { auth } = require('express-openid-connect');
 
+
+/********
+ * Config
+ */
+const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors()).use(bodyParser.json()).use('/', require('./routes/index'));
+const config = {
+  authRequired: false, // TODO: set to false when done
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
 
+app.use(cors()).use(bodyParser.json())
+app.use(auth(config));
+app.use((req: { oidc: { isAuthenticated: () => any; }; }, res: { locals: { isAuthenticated: any; }; }, next: () => void) => {
+  res.locals.isAuthenticated = req.oidc.isAuthenticated();
+  next();
+});
+
+
+
+/*********
+ * Routes 
+ */
+app.use('/', require('./routes/index'));
+
+// > Authorization - TODO: move to a separate file
+app.get('/authorized', function (_req: any, res: { send: (arg0: string) => void; }) {
+  res.send('Secured Resource');
+});
+
+// > Sign up
+app.get('/sign-up', (_req: any, res: { oidc: { login: (arg0: { authorizationParams: { screen_hint: string; }; }) => void; }; }) => {
+  res.oidc.login({
+    authorizationParams: {
+      screen_hint: 'signup',
+    },
+  });
+});
+
+
+
+/********
+ * Connect database and start server
+ */
 mongodb.initDb((err: any, mongodb: any) => {
   if (err) {
     console.log(err);
